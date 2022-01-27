1d2a5a6571ce6feb254436505686431ed21998a5
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-01-26 21:46:37 -0300

    Conceitos de javaScript intermediário

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 /*javaScript Intermediário*/
 
+//Seletores
 
+//SELETOR POR ID
+document.querySelector('#titulo').innerHTML = 'Exemplo'
+
+// SELETOR POR TAG
+document.querySelector('a').innerHTML = 'Teste ancôra'
+
+//SELECIONANDO MAIS DE 1 ITEM POR TAG
+let ancoras = document.querySelectorAll('a')
+
+ancoras.forEach(function (elemento) { //função anonima / callback
+  elemento.innerHTML = 'Teste'
+})
+
+//SELECIONANDO MAIS DE 1 ITEM POR CLASSE
+let boxes = document.querySelectorAll('.box')
+
+boxes.forEach(function (box, index) {
+   box.innerHTML = 'Box ' + (index + 1)
+})
