2d2009c7917b5c5222908b7ce1b102be2bb80324
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-01-27 22:36:02 -0300

    Conceitos de javaScript intermediário - eventos

---
+++ eventos.js
@@ -0,0 +1,10 @@
+let botaoTeste = document.querySelector('#botaoTeste')
+
+botaoTeste.onclick = function () {
+  console.log('Mensagem 1')
+}
+
+botaoTeste.onclick = function () {
+  console.log('Mensagem 2')
+}
+
