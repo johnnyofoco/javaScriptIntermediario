0e17a3a9a5a356e3cc8b3b541c04154759497a04
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-01-29 14:40:42 -0300

    Conceitos de javaScript intermediário - Exercícios

--- ex002/ex002.js
+++ ex002/ex002.js
@@ -1,25 +1,46 @@
-
 //Elementos HTML
 let contador = document.querySelector('.contador')
 let btnSubtrair = document.querySelector('#btnSubtrair')
 let btnSomar = document.querySelector('#btnSomar')
 
 //Variáveis
-let cont = 0
+let count = 0
+let tipoCount = ''
+let intervalo
 
 //EventListener´s
-btnSomar.addEventListener('click', incrementarContador)
 btnSubtrair.addEventListener('click', decrementarContador)
+btnSomar.addEventListener('click', incrementarContador)
 
 //Funções
-function incrementarContador() {
-  cont++
-  contador.innerHTML = `${cont}`
+function definirIntervalo (tipo) {
+  if (tipo === 'decrementar') {
+    intervalo = setInterval(function () {
+      count--
+      contador.innerText = count
+    }, 100)
+  } else if (tipo === 'incrementar') {
+    intervalo = setInterval(function () {
+      count++
+      contador.innerText = count
+    }, 100)
+  } else {
+    clearInterval(intervalo)
+  }
 }
 
 function decrementarContador () {
-  cont--
-  contador.innerHTML = `${cont}`
+  clearInterval(intervalo)
+  btnSomar.classList.remove('somarSelecionado')
+  btnSubtrair.classList.add('subtrairSelecionado')
+  tipoCount = 'decrementar'
+  definirIntervalo(tipoCount)
 }
 
-
+function incrementarContador () {
+  clearInterval(intervalo)
+  btnSubtrair.classList.remove('subtrairSelecionado')
+  btnSomar.classList.add('somarSelecionado')
+  tipoCount = 'incrementar'
+  definirIntervalo(tipoCount)
+}
